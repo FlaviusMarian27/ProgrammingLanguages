1221d38dd121f3858c53322eae01457906af46ed
FlaviusMarian27 <marianflavius27@yahoo.com>
2025-07-29 15:22:54 +0300

    Problema

---
+++ Python/code/arguments.py
@@ -0,0 +1,19 @@
+def myFun(*argv):
+    for arg in argv:
+        print(arg)
+
+myFun('Hello', 'Welcome', 'to', 'GeeksforGeeks')
+
+def fun(arg1, *argv):
+    print("First argument :", arg1)
+    for arg in argv:
+        print("Argument *argv :", arg)
+
+fun('Hello', 'Welcome', 'to', 'GeeksforGeeks')
+
+def fun(**kwargs):
+    for k, val in kwargs.items():
+        print("%s == %s" % (k, val))
+
+# Driver code
+fun(s1='Geeks', s2='for', s3='Geeks')
